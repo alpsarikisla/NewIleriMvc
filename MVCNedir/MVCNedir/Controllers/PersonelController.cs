a9c9d221ff34c386e7a05e852ef19e50fbc87b8d
Alp Sarıkışla <50963484+alpsarikisla@users.noreply.github.com>
2022-03-05 10:18:05 +0300

    Tamamlandi

---
+++ MVCNedir/MVCNedir/Controllers/PersonelController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCNedir.Models;
+
+namespace MVCNedir.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        public ActionResult Index()
+        {
+            List<Personel> personeller = new List<Personel>();
+            personeller.Add(new Personel() { ID= 1, Isim="Ogün", Soyad="Güngör"});
+            personeller.Add(new Personel() { ID= 2, Isim = "Göksel", Soyad = "Doğan" });
+            personeller.Add(new Personel() { ID= 3, Isim = "Doğukan", Soyad = "Kıraslan" });
+            ViewBag.title = "Personel Listesi";
+            return View(personeller);
+        }
+
+        [HttpGet]
+        public ActionResult Kaydet()
+        {
+            //ViewBag.veri = "Selam Dostlar";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Kaydet(Personel p)
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCNedir/MVCNedir/Models/Personel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCNedir.Models
+{
+    public class Personel
+    {
+        public int ID { get; set; }
+        public string Isim { get; set; }
+        public string Soyad { get; set; }
+    }
+}
